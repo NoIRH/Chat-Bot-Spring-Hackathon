04453111b3f2923538f477a0b5f799b91f248ed3
Victor <tema123.ss98@gmail.com>
2023-04-24 02:10:55 +0300

    add manage form

--- GameBot/Controllers/Controllers/BaseController.cs
+++ GameBot/Controllers/Controllers/BaseController.cs
@@ -5,14 +5,18 @@ namespace Controllers.Controllers
     public class BaseController
     {
         public List<User> Users { get; set; }
+        public List<Statistic> Statistics { get; set; }
+
         public BaseController() { } 
         public BaseController(DBController dB)
         {
             Users = dB.GetUsers();
+
         }
         public User GetUser(int id)
         {
             return Users.FirstOrDefault(u => u.Id == id);
         }
+
     }
 }
